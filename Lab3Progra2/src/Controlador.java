b8130b2a2cdc979b6cfe27f08a80047b4eb438a1
David Estrada <david_edgardo@unitec.edu>
2026-08-08 10:24:39 -0600

    Controlador terminado

--- Lab3Progra2/src/Controlador.java
+++ Lab3Progra2/src/Controlador.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Controlador implements LogicaJuego, GestionTurnos {
 
     private Jugador jugador1;
@@ -20,6 +22,80 @@ public class Controlador implements LogicaJuego, GestionTurnos {
         jugador2.reiniciarPuntos();
         jugadorActual = jugador1;
         parejasEncontradas = 0;
+
+        Carta[] cartas = crearCartas();
+
+        mezclarCartas(cartas);
+
+        cargarTablero(cartas);
+    }
+
+    private Carta[] crearCartas() {
+
+        Carta[] cartas = new Carta[36];
+
+        String[] normales = {"bulbasaur", "charmander", "dwebble", "eevee", "lickitung", "pajaro1", "pajaro2", "pajaro3", "rata1", "rata2", "serpiente1", "serpiente2"};
+
+        String[] especiales = {"blastoise", "charmeleon", "clefable", "haunter", "pikachu", "raichu"};
+
+        String rutaOculta = "/Imagenes/pantalla.png";
+
+        int posicion = 0;
+
+        for (int i = 0; i < normales.length; i++) {
+
+            String rutaFrente = "/Imagenes/" + normales[i] + ".png";
+
+            cartas[posicion] = new CasillaNormal(normales[i], rutaFrente, rutaOculta, 100, 100);
+            posicion++;
+
+            cartas[posicion] = new CasillaNormal(normales[i], rutaFrente, rutaOculta, 100, 100);
+            posicion++;
+        }
+
+        for (int i = 0; i < especiales.length; i++) {
+
+            String rutaFrente = "/Imagenes/" + especiales[i] + ".png";
+
+            cartas[posicion] = new CasillaEspecial(especiales[i], rutaFrente, rutaOculta, 100, 100);
+            posicion++;
+
+            cartas[posicion] = new CasillaEspecial(especiales[i], rutaFrente, rutaOculta, 100, 100);
+            posicion++;
+        }
+
+        return cartas;
+    }
+
+    private void mezclarCartas(Carta[] cartas) {
+
+        Random random = new Random();
+
+        for (int i = 0; i < cartas.length; i++) {
+
+            int posicion = random.nextInt(cartas.length);
+
+            Carta temporal = cartas[i];
+            cartas[i] = cartas[posicion];
+            cartas[posicion] = temporal;
+        }
+    }
+
+    private void cargarTablero(Carta[] cartas) {
+
+        int posicion = 0;
+
+        for (int fila = 0; fila < 6; fila++) {
+
+            for (int columna = 0; columna < 6; columna++) {
+
+                tablero[fila][columna] = cartas[posicion];
+
+                tablero[fila][columna].ocultar();
+
+                posicion++;
+            }
+        }
     }
 
     @Override
@@ -29,6 +105,7 @@ public class Controlador implements LogicaJuego, GestionTurnos {
 
     @Override
     public void cambiarTurno() {
+
         if (jugadorActual == jugador1) {
             jugadorActual = jugador2;
         } else {
@@ -38,6 +115,7 @@ public class Controlador implements LogicaJuego, GestionTurnos {
 
     @Override
     public boolean verificarPareja(Carta carta1, Carta carta2) {
+
         if (carta1.esParejaCon(carta2)) {
             return true;
         }
@@ -46,14 +124,29 @@ public class Controlador implements LogicaJuego, GestionTurnos {
     }
 
     public void registrarPareja(Carta carta1, Carta carta2) {
+
         if (verificarPareja(carta1, carta2)) {
+
             jugadorActual.sumarPunto(carta1);
+
             parejasEncontradas++;
+
+            carta1.setEnabled(false);
+            carta2.setEnabled(false);
         }
+    }
+
+    public void parejaIncorrecta(Carta carta1, Carta carta2) {
+
+        carta1.ocultar();
+        carta2.ocultar();
+
+        cambiarTurno();
     }
 
     @Override
     public boolean juegoTerminado() {
+
         if (parejasEncontradas == 18) {
             return true;
         }
@@ -63,17 +156,35 @@ public class Controlador implements LogicaJuego, GestionTurnos {
 
     @Override
     public String finalizarJuego() {
+
         if (jugador1.getPuntos() > jugador2.getPuntos()) {
+
             return "Ganador: " + jugador1.getNombre() + " con " + jugador1.getPuntos() + " puntos.";
+
         } else if (jugador2.getPuntos() > jugador1.getPuntos()) {
+
             return "Ganador: " + jugador2.getNombre() + " con " + jugador2.getPuntos() + " puntos.";
+
         } else {
+
             return "Empate con " + jugador1.getPuntos() + " puntos.";
         }
     }
 
     public Carta getCarta(int fila, int columna) {
-        return tablero[fila][columna];
+
+        try {
+
+            return tablero[fila][columna];
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+            return null;
+        }
+    }
+
+    public Carta[][] getTablero() {
+        return tablero;
     }
 
     public Jugador getJugador1() {
