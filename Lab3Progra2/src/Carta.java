f64ee2b24dbdac48916b3916458931d58b049a71
DiegoR <diegoerecarte@gmail.com>
2026-08-08 09:12:28 -0600

    feat: clase abstracta casilla

--- Lab3Progra2/src/Carta.java
+++ Lab3Progra2/src/Carta.java
@@ -8,6 +8,73 @@
  *
  * @author oscar
  */
+
+import javax.swing.*;
+import java.awt.*;
 public class Carta {
     
+    public abstract class Casilla extends JButton {
+        protected String id;
+        protected String rutaFrente;
+        protected String rutaOculta;
+        protected boolean descubierta;
+        protected int alto;
+        protected int ancho;
+
+        protected Casilla(String id, String rutaFrente, String rutaOculta, int alto, int ancho) {
+            this.id = id;
+            this.rutaFrente = rutaFrente;
+            this.rutaOculta = rutaOculta;
+            this.descubierta = false;
+            this.alto = alto;
+            this.ancho= ancho;
+            
+
+            setFocusPainted(false);
+            setBorderPainted(true);
+        
+
+            Mostrar();
+        }
+
+        public boolean IsDescubierta() {
+            return descubierta;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void Mostrar() {
+            
+            setIcon(escalarImagen(rutaFrente));
+            descubierta = true;
+            setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+            setText("");
+        }
+
+        public void ocultar() {
+            setIcon(escalarImagen(rutaOculta));
+            descubierta = false;
+            setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+            setText("");
+        }
+
+        public boolean esParejaCon(Casilla otra) {
+            boolean pareja;
+            pareja = otra != null && id.equals(otra.getId());
+            return pareja;
+        }
+
+        public abstract int getPuntaje();
+
+        protected ImageIcon escalarImagen(String ruta) {
+            ImageIcon origen = new ImageIcon(getClass().getResource(ruta));
+            Image escala = origen.getImage().getScaledInstance(ancho, alto, Image.SCALE_SMOOTH);
+            return new ImageIcon(escala);
+        }
+    }
+    
 }
+    
+
