a7db742f972628354861eb9eeb76a40fef76637a
Oscarcs92004 <oscarcs92004@gmail.com>
2026-08-08 11:46:49 -0600

    agrego el mensaje de turno

--- Lab3Progra2/src/GUI.java
+++ Lab3Progra2/src/GUI.java
@@ -39,12 +39,14 @@ public class GUI extends JFrame{
     private JLabel jug1Aciertos;
     private JLabel jug2;
     private JLabel jug2Aciertos;
+    private JLabel turno;
     
     public void actualizarPuntaje(){
         jug1.setText(controlador.getJugador1().getNombre() + " - Puntos: " + controlador.getJugador1().getPuntos());
         jug1Aciertos.setText("Cantidad de aciertos de "+ controlador.getJugador1().getNombre()+ " : " + + controlador.getJugador1().getCantAciertos());
         jug2.setText(controlador.getJugador2().getNombre() + " - Puntos: " + controlador.getJugador2().getPuntos());
         jug2Aciertos.setText("Cantidad de aciertos de "+ controlador.getJugador2().getNombre()+ " : " + + controlador.getJugador2().getCantAciertos());
+        turno.setText("Turno de: " + controlador.obtenerJugadorActual().getNombre());
         
         panelJuego.revalidate();
         panelJuego.repaint();
@@ -71,18 +73,22 @@ public class GUI extends JFrame{
         panelJuego.setLayout(new BorderLayout(10,10));
         
         JPanel panelJugadores = new JPanel(new GridLayout(2,2));
+        JPanel panelSuperior = new JPanel(new BorderLayout());
         
         jug1 = new JLabel(controlador.getJugador1().getNombre() + " - Puntos: " + controlador.getJugador1().getPuntos(), SwingConstants.CENTER);
         jug1Aciertos = new JLabel("Cantidad de aciertos de "+ controlador.getJugador1().getNombre()+ " : " + + controlador.getJugador1().getCantAciertos(), SwingConstants.CENTER);
-        jug2 = new JLabel(controlador.getJugador1().getNombre() + " - Puntos: " + controlador.getJugador1().getPuntos(), SwingConstants.CENTER);
+        jug2 = new JLabel(controlador.getJugador2().getNombre() + " - Puntos: " + controlador.getJugador2().getPuntos(), SwingConstants.CENTER);
         jug2Aciertos = new JLabel("Cantidad de aciertos de "+ controlador.getJugador2().getNombre()+ " : " + + controlador.getJugador2().getCantAciertos(), SwingConstants.CENTER);
-       
+        turno = new JLabel("Turno de: " + controlador.obtenerJugadorActual().getNombre(),SwingConstants.CENTER);
+        turno.setFont(new Font("Arial", Font.BOLD, 18));
         panelJugadores.add(jug1Aciertos);
         panelJugadores.add(jug2Aciertos);
         panelJugadores.add(jug1);
         panelJugadores.add(jug2);
         
-        panelJuego.add(panelJugadores, BorderLayout.NORTH);
+        panelSuperior.add(panelJugadores, BorderLayout.CENTER);
+        panelSuperior.add(turno, BorderLayout.SOUTH);
+        panelJuego.add(panelSuperior, BorderLayout.NORTH);
         
         inicializartablero(controlador.getTablero(),900,650);
         
