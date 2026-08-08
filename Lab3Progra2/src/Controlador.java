feeb63e38f6ddae28f8ac24b8cf68ecadfc799e0
David Estrada <david_edgardo@unitec.edu>
2026-08-08 09:46:34 -0600

    Mas avances en controlador

--- Lab3Progra2/src/Controlador.java
+++ Lab3Progra2/src/Controlador.java
@@ -3,15 +3,23 @@ public class Controlador implements LogicaJuego, GestionTurnos {
     private Jugador jugador1;
     private Jugador jugador2;
     private Jugador jugadorActual;
+    private Carta[][] tablero;
     private int parejasEncontradas;
-    private int totalParejas;
 
     public Controlador(String nombre1, String nombre2) {
         jugador1 = new Jugador(nombre1);
         jugador2 = new Jugador(nombre2);
         jugadorActual = jugador1;
+        tablero = new Carta[6][6];
         parejasEncontradas = 0;
-        totalParejas = 18;
+    }
+
+    @Override
+    public void iniciarJuego() {
+        jugador1.reiniciarPuntos();
+        jugador2.reiniciarPuntos();
+        jugadorActual = jugador1;
+        parejasEncontradas = 0;
     }
 
     @Override
@@ -29,22 +37,54 @@ public class Controlador implements LogicaJuego, GestionTurnos {
     }
 
     @Override
-    public void iniciarJuego() {
+    public boolean verificarPareja(Carta carta1, Carta carta2) {
+        if (carta1.esParejaCon(carta2)) {
+            return true;
+        }
 
+        return false;
     }
 
-    @Override
-    public boolean verificarPareja(Carta carta1, Carta carta2) {
-        return false;
+    public void registrarPareja(Carta carta1, Carta carta2) {
+        if (verificarPareja(carta1, carta2)) {
+            jugadorActual.sumarPunto(carta1);
+            parejasEncontradas++;
+        }
     }
 
     @Override
     public boolean juegoTerminado() {
+        if (parejasEncontradas == 18) {
+            return true;
+        }
+
         return false;
     }
 
     @Override
     public String finalizarJuego() {
-        return "";
+        if (jugador1.getPuntos() > jugador2.getPuntos()) {
+            return "Ganador: " + jugador1.getNombre() + " con " + jugador1.getPuntos() + " puntos.";
+        } else if (jugador2.getPuntos() > jugador1.getPuntos()) {
+            return "Ganador: " + jugador2.getNombre() + " con " + jugador2.getPuntos() + " puntos.";
+        } else {
+            return "Empate con " + jugador1.getPuntos() + " puntos.";
+        }
+    }
+
+    public Carta getCarta(int fila, int columna) {
+        return tablero[fila][columna];
+    }
+
+    public Jugador getJugador1() {
+        return jugador1;
+    }
+
+    public Jugador getJugador2() {
+        return jugador2;
+    }
+
+    public int getParejasEncontradas() {
+        return parejasEncontradas;
     }
 }
